36df8b3fd58bd52fb3b9a6fd977c83985998d9d2
Vladicbl <Vladicbl@yandex.ru>
2018-03-03 15:15:06 +0300

    Main scene renamed to GamePlayScene. Added MainMenuScene.

--- Assets/Scripts/GameInit.cs
+++ Assets/Scripts/GameInit.cs
@@ -8,75 +8,121 @@ public class GameInit : MonoBehaviour {
     public List<GameObject> sphere1;
 
     void Start () {
-        StartCoroutine(Lol());
+        StartCoroutine(Init());
     }
 
-    IEnumerator Lol()
+    void Update()
     {
-        float x, y;
+
+    }
+
+    IEnumerator Init()
+    {
+        
         for (int i = 0; i < 5; i++)
         {
-            x = Random.Range(-2.3f, 2.3f);
-            y = Random.Range(-4.3f, 4.3f);
+            
             sphere1.Add(Instantiate(sphere, new Vector3(x, y, 0), new Quaternion()));
             sphere1[i].name = "Sphere"/* + i.ToString*/;
             yield return new WaitForSeconds(1f);
+            actors.add(new SphereActor(sampleCandidate(), 40));
+            stage.addActor(actors.get(i));
         }
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
-//	Random a = new Random ();
 
-//    random = new RandomXS128();
-//    actors = new ArrayList<SphereActor>();
-//	actors.add(new SphereActor(new Vector2((float) random.nextInt(11) + 10, (float) random.nextInt(11) + 10), 40));     //ПЕРВАЯ ТОЧКА
-//	stage.addActor(actors.get(0));
-//	for (byte i = 1; i< 10; i++) {
-//		actors.add(new SphereActor(sampleCandidate(), 40));
-//		stage.addActor(actors.get(i));
-//	}
-//}
+    private Vector3 sampleCandidate()
+    {
+        float x, y;
+        
+        byte numCandidates = 10;
+        Vector3 candidate = new Vector3();
+        Vector3 bestCandidate = new Vector3();
+        float bestDistance = 0f;
+        float distance;
+        x = Random.Range(-2.3f, 2.3f);
+        y = Random.Range(-4.3f, 4.3f);
+        for (byte i = 0; i < numCandidates; i++)
+        {
+            candidate.Set(Random.Range(-2.3f, 2.3f), Random.Range(-2.3f, 2.3f),0);
+            distance = candidate.dst(findClosest(candidate));
+            candidate.
+            if (distance > bestDistance)
+            {
+                bestCandidate.set(candidate.x, candidate.y);
+                bestDistance = distance;
+            }
+        }
+        return bestCandidate;
+    }
 
-//private Vector2 sampleCandidate() {  //МЕТОД ПОДБОРА КАНДИДАТОВ.
-//	byte numCandidates = 100;
-//	Vector2 candidate = new Vector2();
-//	Vector2 bestCandidate = new Vector2();
-//	float bestDistance = 0f;
-//	float distance;
-//	for (byte i = 0; i < numCandidates; i++) {
-//		candidate.set((float) random.nextInt(WIDTH) + 10, (float) random.nextInt(HEIGHT) + 10);
-//		distance = candidate.dst(findClosest(candidate));
-//		if (distance > bestDistance) {
-//			bestCandidate.set(candidate.x, candidate.y);
-//			bestDistance = distance;
-//		}
-//	}
-//	return bestCandidate;
-//}
-
-//private Vector2 findClosest(Vector2 sample) {    // МЕТОД НАХОЖДЕНИЯ БЛИЖАЙШЕГО КАНДИДАТА К ТЕКУЩЕМУ.
-//	float bestDistance = 1000f;
-//	Vector2 closest = new Vector2();
-//	for (byte i = 0; i < actors.size(); i++) {
-//		if (sample.dst(actors.get(i).getSphereVector()) < bestDistance) {
-//			bestDistance = sample.dst(actors.get(i).getSphereVector());
-//			closest = actors.get(i).getSphereVector();
-//		}
-//	}
-//	return closest;
-//}
+    private Vector3 findClosest(Vector3 sample)
+    {
+        float bestDistance = 10f;
+        Vector3 closest = new Vector3();
+        for (byte i = 0; i < actors.size(); i++)
+        {
+            if (sample.dst(actors.get(i).getSphereVector()) < bestDistance)
+            {
+                bestDistance = sample.dst(actors.get(i).getSphereVector());
+                closest = actors.get(i).getSphereVector();
+            }
+        }
+        return closest;
+    }
 
 
-// BLURE - https://github.com/mattdesl/lwjgl-basics/wiki/ShaderLesson5
-//boolean npotSupported = Gdx.graphics.supportsExtension("GL_OES_texture_npot")
-//		|| Gdx.graphics.supportsExtension("GL_ARB_texture_non_power_of_two"); // chek support of NPOT images																	//создание сфер
-//NOTE use 1024x1024 textures or lesser.
-//abs((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))  расстояние между 2 точками.
-// swipe https://github.com/mattdesl/lwjgl-basics/wiki/LibGDX-Finger-Swipe
-// signing your apk https://developer.android.com/studio/publish/app-signing.html
-// https://vk.com/gdevs  ПАБЛИК ПО ГЕЙМДЕВУ.
+    // Организация взаимодействия между скриптами https://habrahabr.ru/post/212055/
+    // блюр который сейчас используется https://www.youtube.com/watch?v=YKTjVACAfqE
+    //	Random a = new Random ();
+
+    //    random = new RandomXS128();
+    //    actors = new ArrayList<SphereActor>();
+    //	actors.add(new SphereActor(new Vector2((float) random.nextInt(11) + 10, (float) random.nextInt(11) + 10), 40));     //ПЕРВАЯ ТОЧКА
+    //	stage.addActor(actors.get(0));
+    //	for (byte i = 1; i< 10; i++) {
+    //		actors.add(new SphereActor(sampleCandidate(), 40));
+    //		stage.addActor(actors.get(i));
+    //	}
+    //}
+
+    //private Vector2 sampleCandidate() {  //МЕТОД ПОДБОРА КАНДИДАТОВ.
+    //	byte numCandidates = 100;
+    //	Vector2 candidate = new Vector2();
+    //	Vector2 bestCandidate = new Vector2();
+    //	float bestDistance = 0f;
+    //	float distance;
+    //	for (byte i = 0; i < numCandidates; i++) {
+    //		candidate.set((float) random.nextInt(WIDTH) + 10, (float) random.nextInt(HEIGHT) + 10);
+    //		distance = candidate.dst(findClosest(candidate));
+    //		if (distance > bestDistance) {
+    //			bestCandidate.set(candidate.x, candidate.y);
+    //			bestDistance = distance;
+    //		}
+    //	}
+    //	return bestCandidate;
+    //}
+
+    //private Vector2 findClosest(Vector2 sample) {    // МЕТОД НАХОЖДЕНИЯ БЛИЖАЙШЕГО КАНДИДАТА К ТЕКУЩЕМУ.
+    //	float bestDistance = 1000f;
+    //	Vector2 closest = new Vector2();
+    //	for (byte i = 0; i < actors.size(); i++) {
+    //		if (sample.dst(actors.get(i).getSphereVector()) < bestDistance) {
+    //			bestDistance = sample.dst(actors.get(i).getSphereVector());
+    //			closest = actors.get(i).getSphereVector();
+    //		}
+    //	}
+    //	return closest;
+    //}
+
+
+    // BLURE - https://github.com/mattdesl/lwjgl-basics/wiki/ShaderLesson5
+    //boolean npotSupported = Gdx.graphics.supportsExtension("GL_OES_texture_npot")
+    //		|| Gdx.graphics.supportsExtension("GL_ARB_texture_non_power_of_two"); // chek support of NPOT images																	//создание сфер
+    //NOTE use 1024x1024 textures or lesser.
+    //abs((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))  расстояние между 2 точками.
+    // swipe https://github.com/mattdesl/lwjgl-basics/wiki/LibGDX-Finger-Swipe
+    // signing your apk https://developer.android.com/studio/publish/app-signing.html
+    // https://vk.com/gdevs  ПАБЛИК ПО ГЕЙМДЕВУ.
 
 }
