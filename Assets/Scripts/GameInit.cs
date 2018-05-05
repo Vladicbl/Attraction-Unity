9d9cb43549c427b4fded4deb1261ef0999099804
Vladicbl <Vladicbl@yandex.ru>
2018-05-05 18:40:10 +0300

    Added some helpful info in the end of GameInit.cs. Added methods for crossing check. Renamed some methods and properties.

--- Assets/Scripts/GameInit.cs
+++ Assets/Scripts/GameInit.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class GameInit : MonoBehaviour {
 
-    public GameObject sphere;
-    public List<GameObject> sphere1;
+    public GameObject spherePrefab;
+    public GameObject linePrefab;
+    public List<GameObject> sphere;
+    public Sprite sprite;
 
-    void Start () {
+    private readonly float HORIZONTAL_BORDER_LEFT = -2.3f;
+    private readonly float HORIZONTAL_BORDER_RIGHT = 2.3f;
+    private readonly float VERTICAL_BORDER_LEFT = -4.3f;
+    private readonly float VERTICAL_BORDER_RIGHT = 4.3f;
+
+    private bool[] incidenceMatrix = new bool[5]; //5 сфер
+
+    void Start() {
         StartCoroutine(InitGameplay());
+        Debug.Log(sphere[0].active);
+
+        //GameObject.Find("sphere 1").transform.Translate(;
+        //sphere[1].active;
+        //Destroy(sphere[4], 1f);
+        //sphere[0].gameObject.Destr
+        //Debug.Log();
+
+        for (int i = 0; i < length; i++)
+        {
+            
+        }
     }
 
     void Update()
@@ -18,18 +39,29 @@ public class GameInit : MonoBehaviour {
 
     IEnumerator InitGameplay()
     {
-        
-        for (int i = 0; i < 10; i++)
-        {            
-            sphere1.Add(Instantiate(sphere, SampleCandidate(), new Quaternion()));
-            sphere1[i].name = "Sphere " + i.ToString();
+        for (int i = 0; i < 5; i++)
+        {
+            sphere.Add(Instantiate(spherePrefab, SampleCandidate(), new Quaternion()));
+            sphere[i].name = "Sphere " + i.ToString();
+            sphere[0].GetComponent<Animator>().enabled = false;
+            //sphere[i].GetComponent<Animation>().Stop();
+
+            
+
+            //sphere[i].GetComponent<Transform>().forward = 2;
+            //sphere[i].GetComponent<Buttons>().buttonsQuantity; как такое возможно?! ------------------------------
             yield return new WaitForSeconds(1f);
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Intersect(sphere[i].GetComponent<Transform>().position, sphere[i + 1].GetComponent<Transform>().position,
+                sphere[i + 2].GetComponent<Transform>().position, sphere[i + 3].GetComponent<Transform>().position);
         }
     }
 
-
     private Vector3 SampleCandidate()
-    { 
+    {
         byte numCandidates = 20;
 
         float bestDistance = 0f;
@@ -40,41 +72,131 @@ public class GameInit : MonoBehaviour {
 
         for (byte i = 0; i < numCandidates; i++)
         {
-            candidate.Set(Random.Range(-2.3f, 2.3f), Random.Range(-4.3f, 4.3f), 0);
-            distance = Vector3.Distance(candidate, FindClosest(candidate));
+            candidate.Set(Random.Range(HORIZONTAL_BORDER_LEFT, HORIZONTAL_BORDER_RIGHT), Random.Range(VERTICAL_BORDER_LEFT, VERTICAL_BORDER_RIGHT), 0);
+            distance = Vector3.Distance(candidate, FindClosest(ref candidate));
+
             if (distance > bestDistance)
             {
                 bestCandidate.Set(candidate.x, candidate.y, 0);
                 bestDistance = distance;
             }
         }
+
         return bestCandidate;
     }
 
-    private Vector3 FindClosest(Vector3 sample)
+    private Vector3 FindClosest(ref Vector3 sample)
     {
         float bestDistance = 10f;
+
         Vector3 closest = new Vector3();
-        for (byte i = 0; i < sphere1.Count; i++)
+
+        for (byte i = 0; i < sphere.Count; i++)
         {
-            if (Vector3.Distance(sample, sphere1[i].transform.position) < bestDistance)
-            {     
-                bestDistance = Vector3.Distance(sample, sphere1[i].transform.position);
-                closest = sphere1[i].transform.position;
+
+            if (Vector3.Distance(sample, sphere[i].transform.position) < bestDistance)
+            {
+                bestDistance = Vector3.Distance(sample, sphere[i].transform.position);
+                closest = sphere[i].transform.position;
             }
         }
+
         return closest;
     }
 
+    private void CreateLine()
+    {
+
+    }
+
+    //struct pt http://e-maxx.ru/algo/segments_intersection_checking
+    //{
+    //    int x, y;
+    //};
+
+    private float Area(Vector3 firstPoint, Vector3 secondPoint, Vector3 thirdPoint)
+    {
+        return (secondPoint.x - firstPoint.x) * (thirdPoint.y - firstPoint.y) - 
+            (secondPoint.y - firstPoint.y) * (thirdPoint.x - firstPoint.x);
+    }
+
+    private bool Intersect_1(float a, float b, float c, float d)
+    {
+        //a = a + b;
+        //b = a - b;
+        //a = a - b;
+        if (a > b) b = a - (a = b); //swap(a, b);
+        if (c > d) c = d - (d = c); //swap(c, d);
+        return Mathf.Max(a, c) <= Mathf.Min(b, d); //max(a, c) <= min(b, d);
+    }
+    
+    private bool Intersect(Vector3 firstPoint, Vector3 secondPoint, Vector3 thirdPoint, Vector3 fourthPoint)
+    {
+        return Intersect_1(firstPoint.x, secondPoint.x, thirdPoint.x, fourthPoint.x)
+            && Intersect_1(firstPoint.y, secondPoint.y, thirdPoint.y, fourthPoint.y)
+            && Area(firstPoint, secondPoint, thirdPoint) * Area(firstPoint, secondPoint, fourthPoint) <= 0
+            && Area(thirdPoint, fourthPoint, firstPoint) * Area(thirdPoint, fourthPoint, secondPoint) <= 0;
+    }
+
+    //return true;
+    //}
+    //[Header("Name")] разделение переменных
+    //[Space]
+    //[Header("Name")]
 
     // Организация взаимодействия между скриптами https://habrahabr.ru/post/212055/
     // блюр который сейчас используется https://www.youtube.com/watch?v=YKTjVACAfqE
 
-    //boolean npotSupported = Gdx.graphics.supportsExtension("GL_OES_texture_npot")
-    //		|| Gdx.graphics.supportsExtension("GL_ARB_texture_non_power_of_two"); // chek support of NPOT images																	//создание сфер
-    //NOTE use 1024x1024 textures or lesser.
+
 
     // swipe https://github.com/mattdesl/lwjgl-basics/wiki/LibGDX-Finger-Swipe
     // https://vk.com/gdevs  ПАБЛИК ПО ГЕЙМДЕВУ.
+    //public GameObject pr;
+    //public Texture texture;
+    //pr.GetComponent<SpriteRenderer>().sprite = Sprite.Create((Texture2D)texture,new Rect(448,448,32,32), new Vector2(0,0),100f);
 
+    //Button but = test.GetComponent<Button>();
+    //but.OnClick.AddListener("вашМетод");
+
+    //https://unity3d.com/ru/how-to/highlights-from-end-to-end-2D-toolset
+    //Заметка для опытных программистов: вы можете быть удивлены, что инициализация объекта выполняется не в функции-конструкторе. Это потому, что создание объектов обрабатывается 
+    //редактором и происходит не в начале игрового процесса, как вы могли бы ожидать. Если вы попытаетесь определить конструктор для скриптового компонента, он будет мешать 
+    //нормальной работе Unity и может вызвать серьезные проблемы с проектом.
+
+    //public class Enemy : MonoBehaviour
+    //{
+    //    public GameObject player;
+
+    //    void Start()
+    //    {
+    //        // Start the enemy ten units behind the player character.
+    //        transform.position = player.transform.position - Vector3.forward * 10f;
+    //    }
+    //}
+
+    //Кроме того, если объявить переменную с доступом public 
+    //и заданным типом компонента в вашем скрипте, вы сможете перетащить любой объект, который содержит
+    //присоединенный компонент такого типа.Это позволит обращаться к компоненту напрямую, а не через игровой объект.
+    //public Transform playerTransform;
+
+    //void Update()
+    //{
+    //    float distance = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+    //    transform.Translate(Vector3.right * distance);
+    //}
+
+
+
+    //public float distancePerSecond;
+
+    //void Update()
+    //{
+    //    transform.Translate(0, 0, distancePerSecond * Time.deltaTime); перемещение объекта. умножать на дельтаТайм, чтобы при любом фпс все было ути-пути.
+        //При применении расчётов передвижения внутри FixedUpdate, вам не нужно умножать ваши значения на Time.deltaTime.
+        //Потому что FixedUpdate вызывается в соответствии с надёжным таймером, независящим от частоты кадров.
+    //    https://docs.unity3d.com/ru/current/Manual/TimeFrameManagement.html
+    //}
+
+    //Основное правило заключается в том, чтобы не было ссылок на скрипты, компилирующиеся в фазе после.
+    //Все, что компилируется в текущей или ранее выполненной фазе, должно быть полностью доступно.
 }
